0d66959ee65165f5ef598081101d91c9bffedcd8
Ahmed <ma7985067@gmail.com>
2023-10-25 12:53:36 +0500

    new

---
+++ app/Http/Controllers/Aboutco2Controller.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Aboutco2Controller extends Controller
+{
+    //
+}
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PDFController;
-use Illuminate\Support\Facades\Storage;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,8 +14,21 @@ use Illuminate\Support\Facades\Storage;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 Route::get('/', function () {
-    return view('frontend.welcome');
+    return view('welcome');
 })->name('welcome');
 Route::get('/insurance', function () {
     return view('frontend.insurance');
@@ -73,6 +86,4 @@ Route::get('/transporteur', function () {
 })->name('transporteur');
 Route::get('/generate-cgv', [PDFController::class, 'generateCGVPDF'])->name('generate-cgv');
 Route::get('/generate-cgy', [PDFController::class, 'generateCGUPDF'])->name('generate-cgu');
-// Route::get('/download/{filename}', [FileDownloadController::class, 'downloadFileByName'])->name('download-by-name');
-// Route::get('/download-cgv', [FileDownloadController::class, 'downloadCGV'])->name('download-cgv');
-// Route::get('/view-pdf-design', [PDFController::class,'viewPDFDesign'])->name('view-pdf-design');
+require __DIR__.'/auth.php';
