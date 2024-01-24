017aae78f2ae329468f00a9b5a8513dc7c5a78a2
uzair <ma7985067@gmail.com>
2024-01-24 16:36:22 +0500

    Update RegisteredUserController.php

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -51,8 +51,9 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
-        Auth::login($user);
+        // Auth::login($user);
 
-        return redirect(RouteServiceProvider::HOME);
+        return redirect()->route('welcome');
+
     }
 }
