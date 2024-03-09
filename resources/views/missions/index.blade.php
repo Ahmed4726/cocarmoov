023a60b6eb27dcf1dda440a2f156f58cdfdbba64
uzair <ma7985067@gmail.com>
2024-03-09 08:53:12 +0500

    new

--- resources/views/missions/index.blade.php
+++ resources/views/missions/index.blade.php
@@ -1,8 +1,8 @@
 @extends('frontend.frontend_main_layout')
 @section('content')
-<br>
-<div class="container-fluid">
-<div class="card">
+<div class="container-fluid pt-3 pb-4" style="background-color: #333132;">
+<h2 class="text-center text-light">Find a Job</h2>
+<div class="card rounded">
     <div class="card-body">
     <form name="search_form" action="/check-missions" method="POST" style="display: flex; gap: 8px; flex-wrap: wrap; align-items: center;">
         <input class="form-control" type="text" name="city_of_collection" id="city_of_collection" placeholder="City of Collection" style="flex: 1;">
@@ -18,38 +18,35 @@
 
 
 <div class="container-fluid">
-<div class="card mt-4">
+<div class="card mt-4 rounded">
     <div class="card-body">
         <div class="container">
-            <table class="table table-responsive">
-                <tr>
-                    <td class="col-md-4">
-                        <p>City: {{ $mission->from_address }}</p>
-                        <p>Delivery: {{ $mission->to_address }}</p>
-                        <table>
-                            <tr>
-                                <td>Departure Period:</td>
-                                <td>{{ $mission->car_move_departure_date_from }} - {{ $mission->car_move_departure_date_to }}</td>
-                            </tr>
-                        </table>
-                    </td>
-                    <td class="col-md-2">Distance: 890 km</td>
-                    <td class="col-md-2">
+            <div class="row">
+                <div class="col-md-12">
+                <div class="row mt-3">
+                    <div class="col-md-2">
+                        <p><b> {{ $mission->from_address }} - {{ $mission->to_address }}</b></p>
+                        <!-- <p>Delivery: </p> -->
+                        </div>
+                        <div class="col-md-2">
+                                <!-- <td>Departure Period:</td> -->
+                                <div class="text-muted">Valid till {{ $mission->car_move_departure_date_from }} - {{ $mission->car_move_departure_date_to }}</div>
+                        </div>
+                    <div class="col-md-2">Distance: 890 km</div>
+                    <div class="col-md-2">
                         <img src="{{ asset('/dist/img/cars/3m3.png') }}" alt="test" class="img-fluid" width="100" height="100">
-                    </td>
-                    <td class="col-md-2">
-                        <table>
-                            <tr>
-                                <td>End's in:</td>
-                                <td>5 Hours</td>
-                            </tr>
-                        </table>
-                    </td>
-                    <td class="col-md-3">
+                    </div>
+                    <div class="col-md-2">
+                                <div>End's in: 5 Hours</div>
+                                
+                    </div>
+                    <div class="col-md-2">
                         <a href="{{ route('booking', ['id' => $mission->id]) }}" class="btn btn-warning">Book Now</a>
-                    </td>
-                </tr>
-            </table>
+                    </div>
+                </div>
+        
+            </div>
+            </div>
         </div>
     </div>
 </div>
